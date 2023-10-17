025977946dd849f1ae8fa7fc5e3f7281d4cfa0f6
kei-lain <laineytubbs@protonmail.com>
2023-10-17 01:00:53 -0700

    encrypting file via aes-256 prior to send works

--- custom-sftp-client-test.py
+++ custom-sftp-client-test.py
@@ -3,6 +3,8 @@ import Cryptodome
 import socket
 import os
 from dotenv import load_dotenv
+from Crypto.Cipher import AES
+from Crypto.Random import get_random_bytes
 
 load_dotenv()
 
@@ -11,6 +13,20 @@ password = os.getenv('password1')
 port1 = os.getenv('port1')
 hostname1 = os.getenv('hostname1')
 
+def pad_data(data):
+    block_size = AES.block_size
+    return data + (block_size - len(data) % block_size) * chr(block_size - len(data) % block_size)
+
+def encrypt_file(filepath, key):
+    cipher = AES.new(key, AES.MODE_EAX)
+    with open(filepath, 'rb') as file:
+        plaintext = file.read()
+    ciphertext, tag = cipher.encrypt_and_digest(plaintext)
+
+    with open(filepath + '.enc', 'wb') as encrypted_file:
+        encrypted_file.write(cipher.nonce)
+        encrypted_file.write(tag)
+        encrypted_file.write(ciphertext)
 
 def Transportation(host, username, password):
     ssh_client = paramiko.SSHClient()
@@ -19,45 +35,16 @@ def Transportation(host, username, password):
     ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
     ssh_client.connect(host, 22 ,username,password)
     ftp_client=ssh_client.open_sftp()
-    filepath = '/home/lain/testftp-file.txt'
-    localpath = '/home/lain/test.txt'
+
+    filepath = '/home/lain/dockerimages.txt.enc'
+    localpath = '/home/lain/dockerimages.txt'
+    key = get_random_bytes(32)
+    encryptedFile = encrypt_file(localpath, key)
+    localpath = (f"{localpath}.enc")
+    print(f"sending {localpath} to {host1}")
     ftp_client.put(localpath, filepath)
     ftp_client.close()
 
-
-    # try:
-       
-    #     # check = sock.connect_ex((host,port))  #attempts to see if the port the user chose is open
-    #     # print()
-    #     # if check == 0 :
-
-    #     #     # ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    #     #     transport = paramiko.Transport((host,port)) 
-            
-
-    #     # else:
-    #     #     print(f'port {port} is not open')
-    #     transport = paramiko.Transport((host,22))
-
-    #     transport.connect(host,username,password)
-    #     # ssh_client.connect(host,port=port,username=username,password=password)
-    #     print("starting transport")
-    #     sftp = paramiko.SFTPClient.from_transport(transport)
-
-    #     ftp = ssh_client.open_sftp()
-    #     filepath = "/"
-    #     localpath = "/home/lain/Pictures/testftp-file.txt"
-    #     sftp.put(localpath,filepath)
-    #     if sftp: sftp.close()
-    #     if transport: transport.close()
-
-
-    
-    # except:
-    #     print("Something went wrong")
-            
-# else:
-#     transport = paramiko.Transport(host, )
 
 
 Transportation(host1, 'root', password)
