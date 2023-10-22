5b6d56fa82f607b2f78590a007464eaee5ccfb0c
kei-lain <laineytubbs@protonmail.com>
2023-10-21 20:10:50 -0500

    setup.sh setup for easy installation of the kyannon script

--- setup.py
+++ setup.py
@@ -1,47 +1,4 @@
-# import setuptools
 
-# with open("README.md", "r", encoding="utf-8") as fhand:
-#     long_description = fhand.read()
-
-# setuptools.setup(
-#     name="Kyannon-a double encryption sftp script",
-#     version="0.0.1",
-#     author="Lainey Tubbs",
-#     author_email="Laineytubbs@protonmail.com",
-#     description=("This cli script allows encrypts files via AES-256 prior to sending it over SFTP then decrypts it on the other side. This prevents sensitive files from being suceptible to packet capturing"),
-#     long_description=("This cli script allows encrypts files via AES-256 prior to sending it over SFTP then decrypts it on the other side. This prevents sensitive files from being suceptible to packet capturing"),
-#     long_description_content_type="text/markdown",
-#     url="https://github.com/kei-lain/kyannon",
-#     project_urls={
-#         # "Bug Tracker": 
-#     classifiers==[
-#         "Programming Language :: Python :: 3",
-#         "License :: OSI Approved :: MIT License",
-#         "Operating System :: OS Independent",
-#     ],
-#     install_requires==["setuptools",
-#     "bcrypt==4.0.1",
-#     "cffi==1.15.1",
-#     "cryptography==41.0.4",
-#     "paramiko==3.3.1",
-#     "pycparser==2.21",
-#     "pycryptodome==3.19.0",
-#     "pycryptodomex==3.19.0",
-#     "PyNaCl==1.5.0",
-#     "python-aes256==1.0.5",
-#     "python-dotenv==1.0.0",
-#     "scp==0.14.5"
-#     ],
-#     packages=setuptools.find_packages(),
-#     python_requires=">=3.6",
-#     entry_points={
-#         "console_scripts": [
-#             "kyannon = kyannon",
-#             "remotedecypt = remotedecrypt",
-#         ]
-#     }
-#     }
-# )
 
 import setuptools
 
@@ -63,18 +20,18 @@ setuptools.setup(
         "Operating System :: OS Independent",
     ],
     install_requires=[
-        "bcrypt==4.0.1",
-        "cffi==1.15.1",
+        "setuptools",
         "cryptography==41.0.4",
-        "paramiko==3.3.1",
-        "pycparser==2.21",
-        "pycryptodome==3.19.0",
-        "pycryptodomex==3.19.0",
-        "PyNaCl==1.5.0",
-        "python-aes256==1.0.5",
-        "python-dotenv==1.0.0",
-        "scp==0.14.5"
-    ],
+        "ccfi",
+    "bcrypt==4.0.1",
+    "paramiko==3.3.1",
+    "pycparser==2.21",
+    "pycryptodomex==3.19.0",
+    "PyNaCl==1.5.0",
+    "python-aes256==1.0.5",
+    "python-dotenv==1.0.0",
+    "scp==0.14.5"
+],
     packages=setuptools.find_packages(),
     python_requires=">=3.6",
     entry_points={
---
+++ setup.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+sudo python -m pip install -r requirements.txt
+sudo cp -r kyannon remotedecrypt.py /bin/
+echo 'voila'
